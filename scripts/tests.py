5f7348c369698fd5099d94adc582e241baa4e2fd
Roberto Parra <uedsoldier1990@gmail.com>
2025-06-18 13:30:53 -0600

    Consistently use single quotes for string literals in tests.py

--- scripts/tests.py
+++ scripts/tests.py
@@ -2,32 +2,32 @@ import subprocess
 
 class BaseTest:
     def __init__(self, tb_name=None):
-        self.clk_freq_mhz = int(input("Enter main clock frequency [MHz]: "))
+        self.clk_freq_mhz = int(input('Enter main clock frequency [MHz]: '))
         self.clk_freq_hz = self.clk_freq_mhz * 1_000_000
         self.clk_period_ns = 1_000_000_000 // self.clk_freq_hz
-        print(f"Main clock period: {self.clk_period_ns} [ns]")
+        print(f'Main clock period: {self.clk_period_ns} [ns]')
 
         self.tb_name = tb_name
         # 📄 Archivos
-        self.src_dir = "src"
-        self.tb_file = f"testbenches/{tb_name}.v"
-        self.output_file = f"sim/{tb_name}.vvp"
+        self.src_dir = 'src'
+        self.tb_file = f'testbenches/{tb_name}.v'
+        self.output_file = f'sim/{tb_name}.vvp'
 
     def verilog_cmd(self):
         # 🚀 Ejecutar
         try:
             result = subprocess.run(self.iverilog_cmd, check=True, capture_output=True, text=True)
-            print(f"✅ Compilation successful. Output: {self.output_file}")
+            print(f'✅ Compilation successful. Output: {self.output_file}')
         except subprocess.CalledProcessError as e:
-            print("❌ Compilation failed.")
-            print("🔧 STDOUT:")
+            print('❌ Compilation failed.')
+            print('🔧 STDOUT:')
             print(e.stdout)
-            print("🔧 STDERR:")
+            print('🔧 STDERR:')
             print(e.stderr)
     
     def build(self):
-        print("🔧 Compiling with:")
-        print(" ".join(self.iverilog_cmd))
+        print('🔧 Compiling with:')
+        print(' '.join(self.iverilog_cmd))
         self.verilog_cmd()
 
     
@@ -35,15 +35,15 @@ class PWMTest(BaseTest):
     def __init__(self,tb_name):
         super().__init__(tb_name)
 
-        self.pwm_freq_hz = int(input("Enter PWM frequency [Hz]: "))
+        self.pwm_freq_hz = int(input('Enter PWM frequency [Hz]: '))
         self.pwm_period = self.clk_freq_hz // self.pwm_freq_hz
         print('PWM parameters:')
         print('\tFrequency: {self.pwm_freq_hz} [Hz]')
         print(f'\tPeriod: {self.pwm_period} [cycles] ({self.pwm_period*self.clk_period_ns} [ns])')
 
-        self.duty_a = int(input("Enter duty cycle A (0-PWM period): "))
-        self.duty_b = int(input("Enter duty cycle B (0-PWM period): "))
-        self.duty_c = int(input("Enter duty cycle C (0-PWM period): "))
+        self.duty_a = int(input('Enter duty cycle A (0-PWM period): '))
+        self.duty_b = int(input('Enter duty cycle B (0-PWM period): '))
+        self.duty_c = int(input('Enter duty cycle C (0-PWM period): '))
 
         self.iverilog_cmd = [
             'iverilog',
@@ -61,9 +61,9 @@ class BLDCsimpleTest(BaseTest):
     def __init__(self,tb_name):
         super().__init__(tb_name)
 
-        self.step_duration_ns = int(input("Enter open-loop step duration [ns]: "))
+        self.step_duration_ns = int(input('Enter open-loop step duration [ns]: '))
         self.step_duration_cycles = self.step_duration_ns // self.clk_period_ns
-        print(f"Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)")
+        print(f'Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)')
         self.iverilog_cmd = [
             'iverilog',
             '-o', self.output_file,
@@ -77,13 +77,13 @@ class HallTest(BaseTest):
     def __init__(self, tb_name):
         super().__init__(tb_name)
 
-        self.hall_simulated_period_ns = int(input("Enter simulated Hall sensor period in [ns]: "))
+        self.hall_simulated_period_ns = int(input('Enter simulated Hall sensor period in [ns]: '))
         self.hall_simulated_period_clk = self.hall_simulated_period_ns // self.clk_period_ns
-        print(f"Simulated Hall sensor period (cycles): {self.hall_simulated_period_clk}")
+        print(f'Simulated Hall sensor period (cycles): {self.hall_simulated_period_clk}')
 
-        self.hall_simulated_strobe_duration_ns = int(input("Enter simulated Hall sensor strobe duration in [ns]: "))
+        self.hall_simulated_strobe_duration_ns = int(input('Enter simulated Hall sensor strobe duration in [ns]: '))
         self.hall_simulated_strobe_duration_clk = self.hall_simulated_strobe_duration_ns // self.clk_period_ns
-        print(f"Simulated Hall sensor strobe duration (cycles): {self.hall_simulated_strobe_duration_clk}")
+        print(f'Simulated Hall sensor strobe duration (cycles): {self.hall_simulated_strobe_duration_clk}')
 
         self.iverilog_cmd = [
             'iverilog',
@@ -98,19 +98,19 @@ class HallTest(BaseTest):
 class BLDCPWMTest(BaseTest):
     def __init__(self, tb_name):
         super().__init__(tb_name)
-        self.pwm_freq_hz = int(input("Enter PWM frequency [Hz]: "))
+        self.pwm_freq_hz = int(input('Enter PWM frequency [Hz]: '))
         self.pwm_period = self.clk_freq_hz // self.pwm_freq_hz
         print('PWM parameters:')
         print('\tFrequency: {self.pwm_freq_hz} [Hz]')
         print(f'\tPeriod: {self.pwm_period} [cycles] ({self.pwm_period*self.clk_period_ns} [ns])')
 
         # All duty cycles must be the same for BLDC PWM
-        self.duty = int(input("Enter duty cycle (0-PWM period): "))
+        self.duty = int(input('Enter duty cycle (0-PWM period): '))
 
 
-        self.step_duration_ns = int(input("Enter open-loop step duration [ns]: "))
+        self.step_duration_ns = int(input('Enter open-loop step duration [ns]: '))
         self.step_duration_cycles = self.step_duration_ns // self.clk_period_ns
-        print(f"Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)")
+        print(f'Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)')
 
 
         self.iverilog_cmd = [

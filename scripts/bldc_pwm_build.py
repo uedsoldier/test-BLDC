fd4a95f4484062a15b89f3f7ad3fcfcf2a2e6565
Roberto Parra <uedsoldier1990@gmail.com>
2025-06-18 13:17:24 -0600

    Add BLDCPWMTest class and corresponding testbench for PWM control

---
+++ scripts/bldc_pwm_build.py
@@ -0,0 +1,5 @@
+from tests import BLDCPWMTest
+
+bldc_pwm= BLDCPWMTest('tb_bldc_pwm')
+# Ejecutar comando iverilog
+bldc_pwm.build()
--- scripts/tests.py
+++ scripts/tests.py
@@ -32,8 +32,8 @@ class BaseTest:
 
     
 class PWMTest(BaseTest):
-    def __init__(self):
-        super().__init__('tb_pwm_generator_3phase' )
+    def __init__(self,tb_name):
+        super().__init__(tb_name)
 
         self.pwm_freq_hz = int(input("Enter PWM frequency [Hz]: "))
         self.pwm_period = self.clk_freq_hz // self.pwm_freq_hz
@@ -57,13 +57,12 @@ class PWMTest(BaseTest):
         ]
 
 class BLDCsimpleTest(BaseTest):
-    def __init__(self):
-        super().__init__('tb_bldc_commutator')
+    def __init__(self,tb_name):
+        super().__init__(tb_name)
 
         self.step_duration_ns = int(input("Enter open-loop step duration [ns]: "))
         self.step_duration_cycles = self.step_duration_ns // self.clk_period_ns
         print(f"Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)")
-
         self.iverilog_cmd = [
             'iverilog',
             '-o', self.output_file,
@@ -73,16 +72,9 @@ class BLDCsimpleTest(BaseTest):
             self.tb_file,
         ]
     
-    
-
-
-        
-        
-
-
 class HallTest(BaseTest):
-    def __init__(self, tb_name=None):
-        super().__init__('tb_hall_simulator')
+    def __init__(self, tb_name):
+        super().__init__(tb_name)
 
         self.hall_simulated_period_ns = int(input("Enter simulated Hall sensor period in [ns]: "))
         self.hall_simulated_period_clk = self.hall_simulated_period_ns // self.clk_period_ns
@@ -99,5 +91,33 @@ class HallTest(BaseTest):
             f'-DMAIN_CLOCK_PERIOD_NS={self.clk_period_ns}',
             f'-DHALL_SENSOR_PERIOD_CLK={self.hall_simulated_period_clk}',
             f'-DHALL_SENSOR_STROBE_DURATION_CLK={self.hall_simulated_strobe_duration_clk}',
+            self.tb_file,
+        ]
+
+class BLDCPWMTest(BaseTest):
+    def __init__(self, tb_name):
+        super().__init__(tb_name)
+        self.pwm_freq_hz = int(input("Enter PWM frequency [Hz]: "))
+        self.pwm_period = self.clk_freq_hz // self.pwm_freq_hz
+        print(f"PWM frequency: {self.pwm_freq_hz} [Hz]")
+        print(f"PWM period: {self.pwm_period} [clk cycles]")
+
+        # All duty cycles must be the same for BLDC PWM
+        self.duty = int(input("Enter duty cycle (0-PWM period): "))
+
+
+        self.step_duration_ns = int(input("Enter open-loop step duration [ns]: "))
+        self.step_duration_cycles = self.step_duration_ns // self.clk_period_ns
+        print(f"Open-loop step duration: {self.step_duration_ns} [ns] ({self.step_duration_cycles} clk cycles)")
+
+
+        self.iverilog_cmd = [
+            'iverilog',
+            '-o', self.output_file,
+            '-I', self.src_dir,
+            f'-DMAIN_CLOCK_PERIOD_NS={self.clk_period_ns}',
+            f'-DSTEP_DURATION_CYCLES={self.step_duration_cycles}',
+            f'-DPWM_PERIOD={self.pwm_period}',
+            f'-DDUTY={self.duty}',
             self.tb_file,
         ]

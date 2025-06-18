85331572292980410c4b81e8f69ee7fe96b5e198
Roberto Parra <uedsoldier1990@gmail.com>
2025-06-18 13:46:56 -0600

    Add BLDCPWMHallTest class with PWM and Hall sensor configuration

--- scripts/tests.py
+++ scripts/tests.py
@@ -126,3 +126,40 @@ class BLDCPWMTest(BaseTest):
             f'-DDUTY={self.duty}',
             self.tb_file,
         ]
+
+class BLDCPWMHallTest(BaseTest):
+    def __init__(self, tb_name):
+        super().__init__(tb_name)
+
+        # PWM configuration
+        self.pwm_freq_hz = int(input('Enter PWM frequency [Hz]: '))
+        self.pwm_period = self.clk_freq_hz // self.pwm_freq_hz
+        print('PWM parameters:')
+        print(f'\tFrequency: {self.pwm_freq_hz} [Hz]')
+        print(f'\tPeriod: {self.pwm_period} [cycles] ({self.pwm_period*self.clk_period_ns} [ns])')
+        self.duty = int(input(f'Enter duty cycle (0-{self.pwm_period}): '))
+        if self.duty < 0 or self.duty >= self.pwm_period:
+            raise ValueError(f'Duty cycle must be in the range [0, {self.pwm_period})')
+
+        # Hall sensor simulation
+        self.hall_simulated_period_ns = int(input('Enter simulated Hall sensor period in [ns]: '))
+        self.hall_simulated_period_clk = self.hall_simulated_period_ns // self.clk_period_ns
+        print(f'Simulated Hall sensor period (cycles): {self.hall_simulated_period_clk}')
+
+        self.hall_simulated_strobe_duration_ns = int(input('Enter simulated Hall sensor strobe duration in [ns]: '))
+        self.hall_simulated_strobe_duration_clk = self.hall_simulated_strobe_duration_ns // self.clk_period_ns
+        print(f'Simulated Hall sensor strobe duration (cycles): {self.hall_simulated_strobe_duration_clk}')
+
+        # BLDC configuration not needed, as it will work with hall sensor simulation
+
+        self.iverilog_cmd = [
+            'iverilog',
+            '-o', self.output_file,
+            '-I', self.src_dir,
+            f'-DMAIN_CLOCK_PERIOD_NS={self.clk_period_ns}',
+            f'-DPWM_PERIOD={self.pwm_period}',
+            f'-DDUTY={self.duty}',
+            f'-DHALL_SENSOR_PERIOD_CLK={self.hall_simulated_period_clk}',
+            f'-DHALL_SENSOR_STROBE_DURATION_CLK={self.hall_simulated_strobe_duration_clk}',
+            self.tb_file,
+        ]
